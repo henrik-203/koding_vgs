5fc5419a4582a6894328932ac7792c36849b5ab3
henrik-203 <79516646+henrik-203@users.noreply.github.com>
2022-04-26 09:20:34 +0200

    update app.js
    said

--- test/app.js/App.js
+++ test/app.js/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {AntDesign, Entypo, Ionicons } from '@expo/vector-icons'
+
+const gray = "#91A1BD"
 
 export default function App() {
+  const NeuMorph = ({children, size, style}) => {
+    return (
+      <View style={styles.topShadow}>
+        <View style={styles.bottomShadow}>
+          <View 
+          style={[
+            styles.inner,
+             {width: size || 40, height: size ||40, borderRadius: size /2 || 40 / 2},
+            
+             ]}
+             ></View>
+        </View>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
-      
+      <SafeAreaView style ={{alignSelf: "stretch"}}>
+        <View style ={{ marginHorizontal: 32, marginTop: 32}}>
+          <View style ={styles.topContainer}>
+              <AntDesign name="arrowleft" size={"20"} color={gray} />
+
+          </View>
+            <Text style={styles.playing}>PLAYING NOW</Text>
+        </View>
+      </SafeAreaView>
       
      
     </View>
@@ -14,7 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DEE9FD',
     alignItems: 'center',
     justifyContent: 'center',
 
