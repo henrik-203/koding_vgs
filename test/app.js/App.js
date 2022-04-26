262ffe3c70a2f22fc572f22979985a1d4fef6093
Wang King <44672628+wangkking@users.noreply.github.com>
2022-04-27 01:29:36 +0200

    ok

--- test/app.js/App.js
+++ test/app.js/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import {AntDesign, Entypo, Ionicons } from '@expo/vector-icons'
+import { StyleSheet, Text, View, SafeAreaView, Slider} from "react-native";
+import {AntDesign, Entypo, Ionicons } from '@expo/vector-icons';
 
 const gray = "#91A1BD"
 
@@ -9,37 +9,111 @@ export default function App() {
     return (
       <View style={styles.topShadow}>
         <View style={styles.bottomShadow}>
-          <View 
-          style={[
-            styles.inner,
-             {width: size || 40, height: size ||40, borderRadius: size /2 || 40 / 2},
-            
-             ]}
-             ></View>
+          <View
+            style={[
+              style.inner,
+              { width: size || 40, height: size || 40, borderRadius: size / 2 || 40 / 2},
+              style
+            ]}
+          >
+            {children}
+          </View>
         </View>
       </View>
     )
   }
-  return (
-    <View style={styles.container}>
-      <SafeAreaView style ={{alignSelf: "stretch"}}>
-        <View style ={{ marginHorizontal: 32, marginTop: 32}}>
-          <View style ={styles.topContainer}> 
-              <NeuMorph>
-                  <AntDesign name="arrowleft" size={"20"} color={gray} />
-              
-              </NeuMorph>
-              
 
-          </View>
+return (
+  <View style={styles.container}>
+    <SafeAreaView style={{ alignSelf: "stretch"}}>
+      <View style={{ marginHorizontal: 32, marginTop: 32 }}>
+        <View style={styles.topContainer}>
+          <NeuMorph size={48}>
+            <AntDesign name="arrowleft" size={20} color={gray} />
+          </NeuMorph>
+
+          <View>
             <Text style={styles.playing}>PLAYING NOW</Text>
+          </View>
+
+          <NeuMorph size={48}>
+            <Entypo name="menu" size={24} color={gray} />
+          </NeuMorph>
         </View>
-      </SafeAreaView>
+
+        <View style={styles.songArtContainer}>
+          <NeuMorph size={300}>
+            <Image source={}/>
+          </NeuMorph>
+        </View>
+
+        <View style={styles.songContainer}>
+          <Text style={styles.title}>Lost it</Text>
+          <Text style={styles.artist}>Flum fi. Vic Menas</Text>
+        </View>
+
+        <View style={StyleSheet.tackContainer}>
+          <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <Text style={styles.time}>1:21</Text>
+            <Text style={styles.time}>3:46:21</Text>
+          </View>
+
+          <Slider
+            minimumValue={0}
+            maximumValue={1}
+            minimumTrackTintColor="#8AAAFF"
+            maximumTrackTintColor="#DAE6F4"
+            thumbTintColor="#7B9BFF"
+          />
+        </View>
+
+        <View style={styles.controlsContainer}>
+          <NeuMorph>
+            <Ionicons name="ios-rewind" size={24} color={gray} />
+          </NeuMorph>
+
+          <NeuMorph>
+            <Ionicons name="ios-pause" size={24} color={gray} />
+          </NeuMorph>
+
+          <NeuMorph>
+            <Ionicons name="ios-fastforward" size={24} color={gray} />
+          </NeuMorph>
+
+        </View>
+
+      </View>
+    </SafeAreaView>
+  </View>
+)
+
+//   return (
+//     <View style={styles.container}>
+//       <SafeAreaView style ={{alignSelf: "stretch"}}>
+//         <View style ={{ marginHorizontal: 32, marginTop: 32}}>
+//           <View style ={styles.topContainer}> 
+//               <NeuMorph size={48}>
+//                   <AntDesign name="arrowleft" size={"20"} color={gray} />
+//               </NeuMorph>
+
+//               <View>
+//                 <Text style={styles.playing}>PLAYING NOW</Text>
+//               </View>
+
+//             <NeuMorph size={48}>
+//               <Entypo name="menu" size={24} color={gray} />
+//             </NeuMorph>
       
-     
-    </View>
-  );
-}
+//       </View>
+
+
+
+      
+//       </View>
+
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
