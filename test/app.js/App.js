50995601e46d6257e51582c8a33bb1b396bc37f8
henrik-203 <79516646+henrik-203@users.noreply.github.com>
2022-04-26 08:39:48 +0200

    update app.js

--- test/app.js/App.js
+++ test/app.js/App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>cato elsker menn</Text>
-      <StatusBar style="auto" />
+      
+      
+     
     </View>
   );
 }
